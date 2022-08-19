6e9ebb5b3d42565f0c6dc6f995bce152f98a07ef
Subin <80036437+hansb1023@users.noreply.github.com>
2022-08-19 17:38:43 +0900

    Component 함수를 재실행시키는 Prop, State 데이터

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-function Header(props){
+import {useState} from 'react';
+
+function Header(props){ 
   console.log('props', props, props.title);
   return <header>
     <h1><a href="/" onClick={(event)=>{
@@ -16,7 +18,7 @@ function Nav(props){
       lis.push(<li key={t.id}>
         <a id={t.id} href={'/read/'+t.id} onClick={event =>{
           event.preventDefault();
-          props.onChangeMode(event.target.id); //event.target = 이벤트를 유발시킨 태그 = a태그
+          props.onChangeMode(Number(event.target.id)); //event.target = 이벤트를 유발시킨 태그 = a태그
         }}>{t.title}</a>
       </li>)
       //REACT가 추적할 근거로서 key값을 부여
@@ -34,23 +36,45 @@ function Article(props){
   </article>
 }
 function App() {
+  // const _mode = useState('REACT');
+  // const mode = _mode[0];
+  // const setMode = _mode[1];
+  const [mode, setMode] = useState('REACT'); //setMode는 state(setMode 외의 이름들도 가능)
+  const [id, setId] = useState('READ');
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
   ]
+  let content = null;
+  if(mode === 'REACT') {
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  }else if(mode === 'READ'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  }
   return (
     <div>
       <Header title="REACT" onChangeMode={()=>{   //arrow function
-        alert('Header');
+        setMode('REACT');
       }}></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
       }}></Nav>
-      <Article title="Welcome" body="Hello, WEB"></Article>
-      <Article title="Hi" body="Hello, REACT"></Article>
+      {content}
     </div>
   );
 }
 
 export default App;
+
+//Prop - Component를 사용하는 외부 사용자를 위한 데이터
+//State - Component를 만드는 내부자를 위한 데이터
+//Prop과 State에 따라 return값이 달라짐
